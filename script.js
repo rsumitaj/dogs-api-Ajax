19624aec203307553dbed6cee2ec25c16da3d059
rsumitaj <sumitrajtech@gmail.com>
2021-07-11 18:34:17 +0530

    fetch image with jQuery

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 
 function fetchRandomDogImage(){
-  var xhrRequest = new XMLHttpRequest();
 
-  xhrRequest.onload = function(){
-    console.log(xhrRequest.response);
-    var responseJSON = JSON.parse(xhrRequest.response);
-    // json.stringify is used to convert json to string
-    var imageUrl = responseJSON.message;
-    $('#dog-image').attr('src',imageUrl);
-  }
+  // var xhrRequest = new XMLHttpRequest();
 
-  xhrRequest.open('get','https://dog.ceo/api/breeds/image/random',true);
-  xhrRequest.send();
+  // xhrRequest.onload = function(){
+  //   console.log(xhrRequest.response);
+  //   var responseJSON = JSON.parse(xhrRequest.response);
+  //   // json.stringify is used to convert json to string
+  //   var imageUrl = responseJSON.message;
+  //   $('#dog-image').attr('src',imageUrl);
+  // }
+  
+  // xhrRequest.open('get','https://dog.ceo/api/breeds/image/random',true);
+  // xhrRequest.send();
+
+  // with jQuery
+
+  $.ajax({
+    url:'https://dog.ceo/api/breeds/image/random',
+    method:'GET',
+    success:function(data){
+
+        var imageUrl = data.message;
+        $('#dog-image').attr('src',imageUrl);
+      }
+    });
+
 };
 
 
